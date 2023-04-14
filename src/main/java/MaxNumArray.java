f0b34208badf15ba722390479026cc2eeb5b5c25
marynanesterenko <marynanesterenko95@gmail.com>
2023-04-14 12:13:33 -0400

    Approach using enchanced for loop added

--- src/main/java/MaxNumArray.java
+++ src/main/java/MaxNumArray.java
@@ -4,19 +4,39 @@ TASK: create an array of random numbers and write the code to find the max numbe
 public class MaxNumArray {
 
     public static void main(String[] args) {
-        // first, lets create an array:
-        int[] num = {1, 2, 3, 4567, 6, 1, 455678, 0, 5};
+        // Example 1 data:
+        int[] givenArray1 = {50, 10, 20, 8, 1, 9, 399, 100};
+        System.out.println("The maximum number in an array is: " + findMax1(givenArray1));
 
-        // second, lets create a variable, and initialize it with the first element of the array
-        int max = num[0];
+        // Example 2 data:
+        int[] givenArray2 = {1, 2, 3, 4567, 6, 1, 455678, 0, 5};
+        System.out.println("The maximum number in an array is: " + findMax2(givenArray1));
 
-        // third, lets loop through an array using the regular for loop (we can also use an enhanced for loop here)
-        for (int i = 0; i < num.length; i++){
+    }
+
+    public static int findMax1 (int[] array){
+
+        // lets create a variable, and initialize it with the first element of the array:
+        int max = array[0];
+
+        // lets loop through an array using the regular for loop (we can also use an enhanced for loop here)
+        for (int i = 0; i < array.length; i++){
             // let's set a condition for each iteration:
-            if (num[i] >= max){
-                max = num[i];
+            if (array[i] >= max){
+                max = array[i];
             }
         }
-        System.out.println("The maximum number in an array is: " + max);
+        return max;
+    }
+
+    public static int findMax2 (int[] array){
+
+        int max = array[0];
+        for (int element : array){
+            if (array[element] >= max){
+                max = array[element];
+            }
+        }
+        return max;
     }
 }
