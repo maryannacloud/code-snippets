0faa542b663aeb3cbcec5a7eec1efb00cabe32fa
marynanesterenko <marynanesterenko.qa@gmail.com>
2024-12-21 15:39:55 -0500

    Merging two arrays practice

---
+++ src/main/java/MergeArrays.java
@@ -0,0 +1,19 @@
+/*
+Write a Java program to merge two arrays of numbers into one.
+ */
+
+
+import java.util.Arrays;
+
+public class MergeArrays {
+    public static void main(String[] args) {
+        int[] array1 = {8, 9, 3, 5, 6};
+        int[] array2 = {4, 7, 2, 8, 6};
+
+        int[] mergedArray = new int[array1.length + array2.length];
+        System.arraycopy(array1, 0, mergedArray, 0, array1.length);
+        System.arraycopy(array2, 0, mergedArray, array1.length, array2.length);
+
+        System.out.println("Merged array: " + Arrays.toString(mergedArray));
+    }
+}
