5906b71576555b2d803817350dfebe5c991e36ad
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2025-08-06 16:21:14 -0400

    New code snippets added

---
+++ src/main/java/Revision5/Number39.java
@@ -0,0 +1,16 @@
+package Revision5;
+
+public class Number39 {
+    /*
+    Print the following pattern:
+    *********
+     *******
+      *****
+       ***
+        *
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Revision6/Homework.java
@@ -0,0 +1,8 @@
+package Revision6;
+
+public class Homework {
+
+    /*
+    1) Number37Completed - Brainstorm on why spacing is not working and we need to have one extra space in above for loop?
+     */
+}
